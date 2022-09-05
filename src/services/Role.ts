1bf4f2149ec05ac019ee52ab2fca2a0e2f91bc66
nangbx <nangbx01@gmail.com>
2022-09-05 09:41:18 +0700

    Add RoleService

---
+++ src/services/Role.ts
@@ -0,0 +1,4 @@
+const save = async ({ user, role }: { user: string; role: string }) => {};
+export default {
+	save,
+};
--- src/services/index.ts
+++ src/services/index.ts
@@ -2,3 +2,4 @@ export { default as ErrorHandling } from "./ErrorHandling";
 export { default as UserService } from "./User";
 export { default as RelationService } from "./Relation";
 export { default as RelationshipNameService } from "./RelationshipName";
+export { default as RoleService } from "./Role";
